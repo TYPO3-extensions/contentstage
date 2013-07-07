1e96b7090b497e9ac9dc68d5ddff1e44421a53cf
Jonas Felix <jonas.felix@typo3.org>
2013-07-07 17:15:58 +0200

    [FEATURE] first commit Fixes #49767
    Change-Id: I30d5395bfd88928a2801b56c333d56b58fee1e8a
    Reviewed-on: https://review.typo3.org/22123
    Reviewed-by: Nils Blattner
    Tested-by: Nils Blattner
    Reviewed-by: Gordon Brüggemann
    Tested-by: Gordon Brüggemann

---
+++ ext_tables.php
@@ -0,0 +1,66 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+if (TYPO3_MODE === 'BE') {
+
+	/**
+	 * Registers a Backend Module
+	 */
+	Tx_Extbase_Utility_Extension::registerModule(
+		$_EXTKEY,
+		'web',	 // Make module a submodule of 'web'
+		'stage',	// Submodule key
+		'',						// Position
+		array(
+			'Content' => 'compare, view, push',
+		),
+		array(
+			'access' => 'user,group',
+			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_stage.xml',
+		)
+	);
+
+	/**
+	 * Registers a Backend Module
+	 */
+	Tx_Extbase_Utility_Extension::registerModule(
+		$_EXTKEY,
+		'web',	 // Make module a submodule of 'web'
+		'stageSnapshots',	// Submodule key
+		'',						// Position
+		array(
+			'Snapshot' => 'list, create, delete, revert',
+		),
+		array(
+			'access' => 'user,group',
+			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_stageSnapshots.xml',
+		)
+	);
+
+	/**
+	 * Registers a Backend Module
+	 */
+	Tx_Extbase_Utility_Extension::registerModule(
+		$_EXTKEY,
+		'web',	 // Make module a submodule of 'web'
+		'stageInit',	// Submodule key
+		'',						// Position
+		array(
+			'Initialize' => 'show, doInitialize',
+		),
+		array(
+			'access' => 'user,group',
+			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_stageInit.xml',
+		)
+	);
+
+}
+
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Content staging');
+
+?>
